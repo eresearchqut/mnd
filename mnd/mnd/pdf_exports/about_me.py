ea17abbd0ff2d94f06fec226f9ca3505cfe535f7
Tamas Szabo <szabtam@gmail.com>
2019-06-14 12:36:27 +0300

    PDF generation: show '' by default instead of None

--- mnd/mnd/pdf_exports/about_me.py
+++ mnd/mnd/pdf_exports/about_me.py
@@ -17,9 +17,9 @@ def generate_pdf_form_fields(registry, patient):
         'pLastName': patient.family_name,
         'pMaidenName': patient.maiden_name,
         'pDOB': patient.date_of_birth.strftime("%d/%m/%Y") if patient.date_of_birth else '',
-        'pPhoneNo': patient.home_phone or '',
-        'pMobile': patient.mobile_phone or '',
-        'pEmail': patient.email or ''
+        'pPhoneNo': patient.home_phone,
+        'pMobile': patient.mobile_phone,
+        'pEmail': patient.email,
     }
     patient_address = patient.home_address
     if patient_address:
@@ -27,13 +27,13 @@ def generate_pdf_form_fields(registry, patient):
             'pSuburb': patient_address.suburb,
             'pAddress': patient_address.address,
             'pPostcode': patient_address.postcode,
-            'pState': _STATE_VALUE_MAPPING.get(patient_address.state, 'QLD')
+            'pState': _STATE_VALUE_MAPPING.get(patient_address.state, 'QLD') if patient_address.state else '',
         })
 
     # TODO
     # dynamic_data = patient.get_dynamic_data(registry)
 
-    return data
+    return {k: '' if v is None else v for k, v in data.items()}
 
 
 def get_pdf_template():
