b1997289412c8548965d7a4303198fa3fbb956c3
Tamas Szabo <szabtam@gmail.com>
2019-09-17 10:47:04 +0300

    Patient Listing small column changes.

--- mnd/mnd/views/mnd_patients_listing.py
+++ mnd/mnd/views/mnd_patients_listing.py
@@ -1,11 +1,15 @@
 from django.utils.translation import ugettext as _
-from django.utils.formats import date_format, time_format
+from django.utils.formats import date_format
 
 from rdrf.views.patients_listing import (
     Column,
-    ColumnFullName, ColumnDateOfBirth,
-    ColumnCodeField, ColumnDiagnosisProgress,
-    ColumnGeneticDataMap, PatientsListingView
+    ColumnCodeField,
+    ColumnContextMenu,
+    ColumnDateOfBirth,
+    ColumnDiagnosisProgress,
+    ColumnFullName,
+    ColumnGeneticDataMap,
+    PatientsListingView
 )
 
 
@@ -16,15 +20,16 @@ class MNDPatientsListingView(PatientsListingView):
             ColumnFullName(_("Patient"), "patients.can_see_full_name"),
             ColumnDateOfBirth(_("Date of Birth"), "patients.can_see_dob"),
             ColumnCodeField(_("Gender"), "patients.can_see_code_field"),
-            ColumnDiagnosisProgress(_("Diagnosis Entry Progress"), "patients.can_see_diagnosis_progress"),
+            ColumnDiagnosisProgress(_("Overall Form Progress"), "patients.can_see_diagnosis_progress"),
             ColumnGeneticDataMap(_("Genetic Data"), "patients.can_see_genetic_data_map"),
-            ColumnLastUpdated(_("Last updated"), "patients.can_see_dob")
+            ColumnDateLastUpdated(_("Date Last Updated"), "patients.can_see_last_updated_at"),
+            ColumnContextMenu(_("Modules"), "patients.can_see_data_modules"),
         ]
         return columns
 
 
-class ColumnLastUpdated(Column):
+class ColumnDateLastUpdated(Column):
     field = "last_updated_overall_at"
 
     def fmt(self, val):
-        return "{} {}".format(date_format(val), time_format(val)) if val is not None else ""
+        return date_format(val) if val is not None else ""
