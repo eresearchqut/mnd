cbef150061dddd3caa4b917ce1991944b02bb450
Alin Treznai <alin.treznai.gemini@gmail.com>
2020-01-14 21:46:55 +0200

    Allow pdf export for carer

--- mnd/mnd/views/pdf_export_view.py
+++ mnd/mnd/views/pdf_export_view.py
@@ -19,9 +19,14 @@ logger = logging.getLogger("registry_log")
 def pdf_export(request, registry_code, patient_id):
     registry = get_object_or_404(Registry, code=registry_code)
     patient = get_object_or_404(Patient, pk=patient_id)
+    user = request.user
 
-    # TODO: might need further refinement, allow carer of patient, clinicians etc.
-    if not (request.user.is_superuser or request.user == patient.user):
+    allowed_user = (
+        user.is_superuser or user == patient.user or user == patient.carer
+    )
+
+    # TODO: might need further refinement for clinicians etc.
+    if not allowed_user:
         return HttpResponseForbidden()
 
     filename = f'About Me MND - {patient.display_name}.pdf'
