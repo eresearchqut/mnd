d8babb860fdbe248afdb39ba88f636a1f1634b4c
Tamas Szabo <szabtam@gmail.com>
2020-03-31 07:32:37 +1100

    Renaming carer to principal caregiver.

--- mnd/mnd/urls.py
+++ mnd/mnd/urls.py
@@ -13,13 +13,13 @@ rdrf_urls = [p for p in rdrf_urlpatterns if not getattr(p, 'app_name', None) == 
 
 urlpatterns = [
     # Any custom URLs go here before we include the TRRF urls
-    re_path(r"^pdf/about_me/(?P<registry_code>\w+)/(?P<patient_id>\d+)/?$", pdf_export, name='about_me'),
-    re_path(r"^(?P<registry_code>\w+)/patient/add/?$", AddPatientView.as_view(), name='patient_add'),
-    re_path(r"^(?P<registry_code>\w+)/patient/(?P<patient_id>\d+)/edit$", PatientEditView.as_view(), name='patient_edit'),
-    re_path(r'^patientslisting/?', MNDPatientsListingView.as_view(), name="patientslisting"),
-    re_path(r'^patient_carer_registration/?$', PatientCarerRegistrationView.as_view(), name="patient_carer_registration"),
-    re_path(r'^carer_operations/?$', CarerOperationsView.as_view(), name="carer_operations"),
-    re_path(r"^(?P<registry_code>\w+)/carer_registration/?$", MNDRegistrationView.as_view(), name='carer_registration'),
+    re_path(r'^pdf/about_me/(?P<registry_code>\w+)/(?P<patient_id>\d+)/?$', pdf_export, name='about_me'),
+    re_path(r'^(?P<registry_code>\w+)/patient/add/?$', AddPatientView.as_view(), name='patient_add'),
+    re_path(r'^(?P<registry_code>\w+)/patient/(?P<patient_id>\d+)/edit$', PatientEditView.as_view(), name='patient_edit'),
+    re_path(r'^patientslisting/?', MNDPatientsListingView.as_view(), name='patientslisting'),
+    re_path(r'^patient_carer_registration/?$', PatientCarerRegistrationView.as_view(), name='patient_carer_registration'),
+    re_path(r'^carer_operations/?$', CarerOperationsView.as_view(), name='carer_operations'),
+    re_path(r'^(?P<registry_code>\w+)/carer_registration/?$', MNDRegistrationView.as_view(), name='carer_registration'),
     re_path(r'^api/v1/', include(('mnd.services.rest.urls.api_urls', 'api_urls'), namespace='v1')),
     re_path(r'^$', LandingView.as_view(), name='landing'),
     re_path(r'', include(rdrf_urls)),
