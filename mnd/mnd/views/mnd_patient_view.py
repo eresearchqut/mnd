9324aac69072cac8aeb117af99d24b391415fd9b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-10-01 16:02:48 +1000

    Rename "Principal Caregiver" to "Carer"

--- mnd/mnd/views/mnd_patient_view.py
+++ mnd/mnd/views/mnd_patient_view.py
@@ -142,7 +142,7 @@ class FormSectionMixin(PatientFormMixin):
                 PatientInsuranceForm, _("Medicare, Health Insurance and Support details"), "patient_insurance", get_insurance_data(patient), request
             ),
             get_section(
-                PrimaryCarerForm, _("Principal Caregiver"), "primary_carer", get_primary_carer(patient), request,
+                PrimaryCarerForm, _("Carer"), "primary_carer", get_primary_carer(patient), request,
                 get_primary_carer_initial_data(patient), patient
             ),
         ])
