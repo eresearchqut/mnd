27c9f10a456679452b476502303f505f90fed740
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-10-06 14:13:32 +1000

    Custom FormView permissions

--- mnd/mnd/urls.py
+++ mnd/mnd/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include
 from django.urls import re_path
 
+from .views.mnd_form_view import MNDFormView
 from .views.mnd_landing_view import LandingView
 from .views.pdf_export_view import pdf_export
 from .views.mnd_patient_view import AddPatientView, PatientEditView
@@ -13,21 +14,32 @@ from rdrf.urls import urlpatterns as rdrf_urlpatterns
 
 rdrf_urls = [p for p in rdrf_urlpatterns if not getattr(p, 'app_name', None) == 'api_urls']
 
+# Any custom URLs go here before we include the TRRF urls
 urlpatterns = [
-    # Any custom URLs go here before we include the TRRF urls
     re_path(r'^pdf/about_me/(?P<registry_code>\w+)/(?P<patient_id>\d+)/?$', pdf_export, name='about_me'),
+
     re_path(r'^(?P<registry_code>\w+)/patient/add/?$', AddPatientView.as_view(), name='patient_add'),
     re_path(r'^(?P<registry_code>\w+)/patient/(?P<patient_id>\d+)/edit$', PatientEditView.as_view(), name='patient_edit'),
     re_path(r'^patientslisting/?', MNDPatientsListingView.as_view(), name='patientslisting'),
+
     re_path(r'^patient_carer_registration/?$', PatientCarerRegistrationView.as_view(), name='patient_carer_registration'),
     re_path(r'^carer_operations/?$', CarerOperationsView.as_view(), name='carer_operations'),
     re_path(r'^(?P<registry_code>\w+)/carer_registration/?$', MNDRegistrationView.as_view(), name='carer_registration'),
+
     re_path(r'^api/v1/', include(('mnd.services.rest.urls.api_urls', 'api_urls'), namespace='v1')),
+
     re_path(r'^mims/product_search', mims_view.product_search, name='mims_product_search'),
     re_path(r'^mims/product_details', mims_view.product_details, name='mims_product_details'),
     re_path(r'^mims/cmi_details', mims_view.cmi_details, name='mims_cmi_details'),
     re_path(r'^mims/pdf_info', mims_view.pdf_proxy, name='mims_cmi_pdf'),
-    re_path(r'^$', LandingView.as_view(), name='landing'),
+
+    re_path(r"^(?P<registry_code>\w+)/forms/(?P<form_id>\w+)/(?P<patient_id>\d+)/(?P<context_id>add)/?$",
+            MNDFormView.as_view(), name='form_add'),
+    re_path(r"^(?P<registry_code>\w+)/forms/(?P<form_id>\w+)/(?P<patient_id>\d+)/(?P<context_id>\d+)?$",
+            MNDFormView.as_view(), name='registry_form'),
+
     re_path(r'^health-check/?$', health_check, name='mnd_health_check'),
+    re_path(r'^$', LandingView.as_view(), name='landing'),
+
     re_path(r'', include(rdrf_urls)),
 ]
---
+++ mnd/mnd/views/mnd_form_view.py
@@ -0,0 +1,22 @@
+import logging
+
+from django.core.exceptions import PermissionDenied
+
+from rdrf.models.definition.models import RegistryForm
+from rdrf.security.security_checks import get_object_or_permission_denied
+from rdrf.views.form_view import FormView
+
+from registry.patients.models import Patient
+
+logger = logging.getLogger(__name__)
+
+
+class MNDFormView(FormView):
+    def registry_permissions_check(self, request, registry_code, form_id, patient_id, context_id):
+        if request.user.is_clinician:
+            form = get_object_or_permission_denied(RegistryForm, pk=form_id)
+            patient = get_object_or_permission_denied(Patient, pk=patient_id)
+
+            # TODO: Use upcoming patient-clinician connection feature
+            if not form.name.startswith("clinical") and patient.clinician != request.user:
+                raise PermissionDenied("You must be set as the patient's clinician to access this form")
