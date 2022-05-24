a92258be41f0359354187b76ee8b64be672ba6a6
Tamas Szabo <szabtam@gmail.com>
2022-05-24 07:46:10 +0300

    TRRFSD-79: Patients list: filter deceased patients

--- mnd/mnd/views/mnd_patients_listing.py
+++ mnd/mnd/views/mnd_patients_listing.py
@@ -10,9 +10,27 @@ from rdrf.views.patients_listing import (
     ColumnFullName,
     PatientsListingView
 )
+from registry.patients.models import LivingStates
 
 
 class MNDPatientsListingView(PatientsListingView):
+
+    def get_template(self):
+        if not self.registries:
+            return super().get_template()
+        return 'rdrf_cdes/mnd_patients_listing.html'
+
+    def build_context(self):
+        context = super().build_context()
+        context['living_status_preselect_value'] = LivingStates.ALIVE
+        return context
+
+    def set_parameters(self, request):
+        super().set_parameters(request)
+
+        valid_living_states = {val for val, _ in LivingStates.CHOICES}
+        living_status = request.POST.get("searchPanes[living_status][0]")
+        self.living_status_filter = living_status if living_status in valid_living_states else None
 
     def get_columns(self):
         columns = [
@@ -21,9 +39,26 @@ class MNDPatientsListingView(PatientsListingView):
             ColumnCodeField(_("Gender"), "patients.can_see_code_field"),
             ColumnDiagnosisProgress(_("Overall Form Progress"), "patients.can_see_diagnosis_progress"),
             ColumnDateLastUpdated(_("Date Last Updated"), "patients.can_see_last_updated_at"),
+            ColumnLivingStatus(_("Living Status"), "patients.can_see_living_status"),
             ColumnContextMenu(_("Modules"), "patients.can_see_data_modules"),
         ]
         return columns
+
+    def apply_filters(self):
+        super().apply_filters()
+        if self.living_status_filter:
+            self.patients = self.patients.filter(living_status=self.living_status_filter)
+
+        def choice_to_option(value, label):
+            return dict(
+                label=str(label),  # enforcing translation of gettext_lazy label
+                value=value,
+                total=self.patients_base.filter(living_status=value).count(),
+                count=self.patients.filter(living_status=value).count())
+
+        self.searchPanesOptions = {
+            "living_status": [choice_to_option(*x) for x in LivingStates.CHOICES]
+        }
 
 
 class ColumnDateLastUpdated(Column):
@@ -31,3 +66,9 @@ class ColumnDateLastUpdated(Column):
 
     def fmt(self, val):
         return date_format(val) if val is not None else ""
+
+
+class ColumnLivingStatus(Column):
+    field = "living_status"
+    sort_fields = ["living_status"]
+    visible = False
