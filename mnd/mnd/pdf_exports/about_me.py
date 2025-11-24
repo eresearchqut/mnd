37b037ed8e04c37b1af6ac43602e21600d960d21
ppettitau <paola.pettit@qut.edu.au>
2025-11-24 13:36:59 +1000

    [ERP-4320] Fix pdf field value for 'Address is same as mine'

--- mnd/mnd/pdf_exports/about_me.py
+++ mnd/mnd/pdf_exports/about_me.py
@@ -222,7 +222,7 @@ def _generate_primary_carer_fields(primary_carer, patient, patient_address):
         "p2HomePhone": primary_carer.home_phone,
         "p2MobilePhone": primary_carer.mobile_phone,
         "p2Lang": _language_mapping(primary_carer.preferred_language),
-        "same_address": primary_carer.same_address,
+        "same_address": _yes_no_off(primary_carer.same_address),
         "p2Interpreter": _yes_no_off(primary_carer.interpreter_required),
     }
     result.update(primary_carer_address(primary_carer, patient_address))
